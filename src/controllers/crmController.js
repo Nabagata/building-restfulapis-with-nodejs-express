43571ce478c40a72e62dcb7ceee489ff9024d16b
Nabagata <ngsaha234@gmail.com>
2020-08-12 10:08:27 +0530

    update delete controller

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -42,7 +42,7 @@ export const updateContact = (req, res) => {
 }
 
 export const deleteContact = (req, res) => {
-    Contact.remove({ _id: req.params.contactID}, (err, contact) => {
+    Contact.deleteOne({ _id: req.params.contactID}, (err, contact) => {
         if (err) {
             res.send(err);
         }
